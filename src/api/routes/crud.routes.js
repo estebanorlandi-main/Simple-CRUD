599f36eca62c35e4dfd56f23fd7d830415a4bbc2
est-orl <estebanorlandi4@gmail.com>
2021-07-19 15:22:14 -0300

    feat: mongodb and main routes

--- src/api/routes/crud.routes.js
+++ src/api/routes/crud.routes.js
@@ -1,8 +1,28 @@
 const Router = require("express").Router();
 
+Router.get("/create", (req, res) => {
+  const data = "Create";
+  res.json({ data });
+});
+
 Router.get("/all", (req, res) => {
-  const hola = "asdf";
-  res.json({ hola });
+  const data = "All";
+  res.json({ data });
+});
+
+Router.get("/one", (req, res) => {
+  const data = "One";
+  res.json({ data });
+});
+
+Router.get("/update", (req, res) => {
+  const data = "Update";
+  res.json({ data });
+});
+
+Router.get("/delete", (req, res) => {
+  const data = "Delete";
+  res.json({ data });
 });
 
 module.exports = Router;
