f5e5159a64648d52317ffddf8a2f169472ade779
est-orl <estebanorlandi4@gmail.com>
2021-07-25 01:01:31 -0300

    feat: starting with search

--- src/public/js/API_Class.js
+++ src/public/js/API_Class.js
@@ -2,24 +2,28 @@ import Card from "./components/Card.js";
 import Paginate from "./components/Paginate.js";
 
 class API {
-  static obj;
-
   constructor(URL) {
     this.URL = URL;
-    if (!this.obj) this.obj = this;
   }
 
   async getProducts(page = 0, inID) {
     const fetch_data = JSON.parse(
       await (await fetch(this.URL + "list/" + page)).text()
     );
+    const products = fetch_data.products;
+    const cards = products.map((prod) => Card(prod)).join("");
+    $(inID).html(cards);
+    Paginate(fetch_data.paginate, products.length);
+  }
+
+  async search(page = 0, value, inID) {
+    const fetch_data = JSON.parse(
+      await (await fetch(this.URL + `search/${value}/${page}`)).text()
+    );
 
     const products = fetch_data.products;
     const cards = products.map((prod) => Card(prod)).join("");
-
     $(inID).html(cards);
-
-    Paginate(fetch_data.paginate, products.length);
   }
 }
 
--- src/public/js/fetchAPI.js
+++ src/public/js/fetchAPI.js
@@ -3,3 +3,8 @@ import API from "./API_Class.js";
 window.api = new API("http://localhost:8080/api/product/");
 
 api.getProducts(0, "#products");
+
+$("#search").keyup((e) => {
+  console.log();
+  api.search(0, e.target.value, "#products");
+});
